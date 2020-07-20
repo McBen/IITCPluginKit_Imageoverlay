f5e1b6927cc58a5fae2ea1c54cd637d5c740e623
McBen <viertelvor12@gmx.net>
2020-07-20 15:28:43 +0200

    Set downloadURL

---
+++ dist/iitc_plugin_ImageOverlay2.meta.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name            IITC plugin: ImageOverlay2
+// @id              iitc_plugin_ImageOverlay2
+// @category        Draw
+// @version         0.0.0
+// @namespace       https://github.com/jonatkins/ingress-intel-total-conversion
+// @updateURL       https://github.com/McBen/IITCPluginKit_Imageoverlay/raw/master/dist/iitc_plugin_ImageOverlay2.meta.js
+// @downloadURL     https://github.com/McBen/IITCPluginKit_Imageoverlay/raw/master/dist/iitc_plugin_ImageOverlay2.user.js
+// @description     Add an image to your map.
+// @include         https://intel.ingress.com/*
+// @author          Hevne
+// ==/UserScript==
