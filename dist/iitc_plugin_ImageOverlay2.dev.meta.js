7164a8fca15260659cf236ab718454d4f109fb98
McBen <viertelvor12@gmx.net>
2024-09-26 12:47:55 +0200

    dependency update

---
+++ dist/iitc_plugin_ImageOverlay2.dev.meta.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name            IITC plugin: ImageOverlay2
+// @id              iitc_plugin_ImageOverlay2
+// @category        Draw
+// @version         0.0.0.240926.124611
+// @namespace       https://github.com/IITC-CE/ingress-intel-total-conversion
+// @updateURL       https://github.com/McBen/IITCPluginKit_Imageoverlay/raw/master/dist/iitc_plugin_ImageOverlay2.user.js/iitc_plugin_ImageOverlay2.dev.meta.js
+// @downloadURL     https://github.com/McBen/IITCPluginKit_Imageoverlay/raw/master/dist/iitc_plugin_ImageOverlay2.user.js/iitc_plugin_ImageOverlay2.dev.user.js
+// @description     Add an image to your map.
+// @match           https://intel.ingress.com/*
+// @author          Hevne
+// ==/UserScript==
--- dist/iitc_plugin_ImageOverlay2.meta.js
+++ dist/iitc_plugin_ImageOverlay2.meta.js
@@ -3,10 +3,10 @@
 // @id              iitc_plugin_ImageOverlay2
 // @category        Draw
 // @version         0.0.0
-// @namespace       https://github.com/jonatkins/ingress-intel-total-conversion
+// @namespace       https://github.com/IITC-CE/ingress-intel-total-conversion
 // @updateURL       https://github.com/McBen/IITCPluginKit_Imageoverlay/raw/master/dist/iitc_plugin_ImageOverlay2.meta.js
 // @downloadURL     https://github.com/McBen/IITCPluginKit_Imageoverlay/raw/master/dist/iitc_plugin_ImageOverlay2.user.js
 // @description     Add an image to your map.
-// @include         https://intel.ingress.com/*
+// @match           https://intel.ingress.com/*
 // @author          Hevne
 // ==/UserScript==
