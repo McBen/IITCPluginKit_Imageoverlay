4e1b085d6dd96362ffd5ffdde312172492f5928e
McBen <viertelvor12@gmx.net>
2024-09-26 12:49:16 +0200

    add version-number

--- dist/iitc_plugin_ImageOverlay2.dev.user.js
+++ dist/iitc_plugin_ImageOverlay2.dev.user.js
@@ -2,7 +2,7 @@
 // @name            IITC plugin: ImageOverlay2
 // @id              iitc_plugin_ImageOverlay2
 // @category        Draw
-// @version         0.0.0.240926.124611
+// @version         1.0.240926.124828
 // @namespace       https://github.com/IITC-CE/ingress-intel-total-conversion
 // @updateURL       https://github.com/McBen/IITCPluginKit_Imageoverlay/raw/master/dist/iitc_plugin_ImageOverlay2.user.js/iitc_plugin_ImageOverlay2.dev.meta.js
 // @downloadURL     https://github.com/McBen/IITCPluginKit_Imageoverlay/raw/master/dist/iitc_plugin_ImageOverlay2.user.js/iitc_plugin_ImageOverlay2.dev.user.js
--- dist/iitc_plugin_ImageOverlay2.meta.js
+++ dist/iitc_plugin_ImageOverlay2.meta.js
@@ -2,7 +2,7 @@
 // @name            IITC plugin: ImageOverlay2
 // @id              iitc_plugin_ImageOverlay2
 // @category        Draw
-// @version         0.0.0
+// @version         1.0
 // @namespace       https://github.com/IITC-CE/ingress-intel-total-conversion
 // @updateURL       https://github.com/McBen/IITCPluginKit_Imageoverlay/raw/master/dist/iitc_plugin_ImageOverlay2.meta.js
 // @downloadURL     https://github.com/McBen/IITCPluginKit_Imageoverlay/raw/master/dist/iitc_plugin_ImageOverlay2.user.js
